6745d21c30396a6a7db53a377bb7b4009da21f81
adOxey <ado.had@gmail.com>
2020-06-23 23:18:09 +0200

    New header image and tweaked styling

--- src/project/Main/LandingPage/LandingPage.js
+++ src/project/Main/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./LandingPage.module.css";
 import HeaderImage from "./HeaderImage/HeaderImage";
+import MiddleSection from "./MiddleSection/MiddleSection";
 
 const LandinPage = () => {
   const { wrapper } = classes;
@@ -8,6 +9,7 @@ const LandinPage = () => {
   return (
     <div className={wrapper}>
       <HeaderImage />
+      <MiddleSection />
     </div>
   );
 };
